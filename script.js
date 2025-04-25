83b54e212161568a10d9885dccd5fa7cf7090306
Said290411 <suprvoron2904@gmail.com>
2025-04-25 02:46:46 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,132 @@
+// 1
+ 
+// alert(5 % 2)
+
+
+
+// 2
+
+// let ranndom = Math.round(Math.random( 10))
+
+//  alert('random son ' + ranndom)
+
+//  console.log(ranndom);
+
+
+
+
+
+ 
+// 3
+
+// let numbeer = ('12.510')
+
+// let javob = Math.round(numbeer)
+// console.log(javob);
+
+
+
+
+
+// 4
+
+// let texts = ('MARS IT SCHOOL')
+ 
+
+// let aswn = texts.length
+
+// console.log(aswn);
+
+
+
+
+
+// 5
+
+// let javvob = ['MARS IT SCHOOL']
+
+// for (let i = 0; i < 10; i++) {
+//     const element = [i];
+//     console.log(javvob);
+    
+    
+// }
+
+
+// 6
+// let harflar = ["a" , "b" , "d" , "e" , "f" , "g" , "h" , "i" , "j" , "k" , "l" , "m" , "n" , "o" , "p" , "q" , "r" , "s" , "t",  "u" , "v" , "x" , "y" , "z" ]
+
+// console.log(harflar[17] + harflar[13] + harflar[8] + harflar[7] + harflar[2] + harflar[0]);
+
+
+
+
+
+
+// 7
+//  let yosh = +prompt('yoshingizni kiriting')
+
+//  yosh = Number(yosh)
+
+
+//  if (yosh > 18) {
+//     console.log(' Siz balagot yoshidasiz');
+   
+//  } else if (yosh === 18) {
+//     console.log( "balogat yoshi miborak");
+  
+//  } else ( yosh < 18) 
+//     console.log('siz balogat yoshiga etmabsiz');
+
+
+
+
+
+// 8
+// let savol = prompt('ismingizni kiriting')
+ 
+// let rese =  savol.split("").reverse().join("")
+
+// console.log(rese);
+
+
+
+
+// 9
+
+// let ism = prompt('ismingizni kiriting')
+// let yosh = prompt('yoshingizni kiriting')
+
+
+// let javob = document.querySelector('.main-wrapper')
+// let p = document.createElement('p')
+
+
+// p.innerText = `Ismingiz: ${ism}, Yoshingiz: ${yosh}`
+
+// p.style.color = 'yellow'
+// p.style.background = 'green'
+// p.style.paddingTop = "20px"
+// p.style.paddingBottom = "20px"
+// p.style.width = "600px"
+// p.style.paddingLeft = "20px"
+
+// document.body.append(p)
+
+
+
+
+
+
+// 10
+
+// let arryy = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+
+// for (let i = 0; i < arryy.length; i++) {
+//     if ( arryy[i] % 2 === 0 ) {
+//         console.log(arryy[i]);
+        
+//     }
+
+// }
